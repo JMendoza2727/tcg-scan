2416b4a736df8bfc877482a3319050368d898704
Jose Mendoza <josemendoza@MacBook-Air-de-Jose.local>
2026-08-24 22:44:52 +0200

    Temporarily disable unstable friends feature

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pokex-shell-v2321";
+const CACHE = "pokex-shell-v2330-no-friends";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
